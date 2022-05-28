33d826bf5aa7c8d89cf36cc115b3da4c3a48181f
AM Darshan <amdarshan.2001@gmail.com>
2022-05-28 17:59:47 +0530

    added products page, show products & their details

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import {AiOutlineGithub,AiOutlineInstagram,AiOutlineTwitter,AiOutlineLinkedin} from 'react-icons/ai'
+import Link from 'next/link'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>
+        2022@ AMD Headphones All rights reververd
+      </p>
+      <p className='icons'>
+        <Link href={'https://www.linkedin.com/in/am-darshan'}>
+            <AiOutlineLinkedin />
+        </Link>
+        <Link href={'#'}>
+            <AiOutlineInstagram />
+        </Link>
+         <Link href={'#'}>
+            <AiOutlineTwitter />
+        </Link>
+         <Link href={'https://github.com/AM-Darshan'} >
+            <AiOutlineGithub />
+        </Link>
+       
+        
+      </p>
+    </div>
   )
 }
 
--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,24 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
-
-const Product = () => {
+import Link from 'next/link'
+import { urlFor } from '../lib/Client'
+const Product = ({product:{image,name,slug,price}}) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`/product/${slug.current}`}>
+          <div className='product-card'>
+            <img 
+              src={urlFor(image && image[0])}
+              height= {250}
+              width= {250}
+              className='product-image'
+            />
+            <p className='product-name'>{name} </p>
+            <p className='product-price'> ${price}</p>
+          </div>
+      </Link>
+    </div>
   )
 }
 
